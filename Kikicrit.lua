5918c1a814fb08d9a77f840f79f895a95b7febef
KikidoraFear <154637862+KikidoraFear@users.noreply.github.com>
2024-03-22 19:24:40 +0100

    added crit healing
    added some sounds

--- Kikicrit.lua
+++ Kikicrit.lua
@@ -1,3 +1,4 @@
+local number_of_sounds = 4
 
 -- for debugging: DEFAULT_CHAT_FRAME:AddMessage("Test")
 local function print(msg)
@@ -14,24 +15,22 @@ parser:RegisterEvent("CHAT_MSG_SPELL_SELF_DAMAGE")
 -- COMBAT DAMAGE events
 parser:RegisterEvent("CHAT_MSG_COMBAT_SELF_HITS")
 
+-- SPELL HEAL events
+parser:RegisterEvent("CHAT_MSG_SPELL_SELF_BUFF")
+parser:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS")
 
--- local function MakeGfindReady(template) -- changes global string to fit gfind pattern
---   template = gsub(template, "%%s", "(.+)") -- % is escape: %%s = %s raw
---   return gsub(template, "%%d", "(%%d+)")
--- end
-
--- local combatlog_patterns = {} -- parser for combat log, order = {source, attack, target, value, school}, if not presenst = nil; parse order matters!!
--- ####### DAMAGE SOURCE:ME TARGET:OTHER
--- combatlog_patterns[14] = {string=MakeGfindReady(SPELLLOGCRITSCHOOLSELFOTHER), order={nil, 1, 2, 3, 4}, kind="dmg"} -- Your %s crits %s for %d %s damage. (parse before %s crits %s for %d %s damage.)
--- combatlog_patterns[16] = {string=MakeGfindReady(SPELLLOGCRITSELFOTHER), order={nil, 1, 2, 3, nil}, kind="dmg"} -- Your %s crits %s for %d. (parse before %s crits %s for %d.)
--- combatlog_patterns[19] = {string=MakeGfindReady(COMBATHITCRITSELFOTHER), order={nil, nil, 1, 2, nil}, kind="dmg"} -- You crit %s for %d.
--- combatlog_patterns[21] = {string=MakeGfindReady(COMBATHITCRITSCHOOLSELFOTHER), order={nil, nil, 1, 2, 3}, kind="dmg"} -- You crit %s for %d %s damage.
+-- Your %s crits %s for %d %s damage.
+-- Your %s crits %s for %d.
+-- You crit %s for %d.
+-- You crit %s for %d %s damage.
+-- Your %s critically heals you for %d.
+-- %s's %s critically heals you for %d.
 
 parser:SetScript("OnEvent", function()
   if arg1 then
-    if string.find(arg1, " crit ") or string.find(arg1, " crits ") then
-      local idx = math.random(1, 16)
-      PlaySoundFile("Interface\\AddOns\\Kikicrit\\wow-"..idx..".mp3")
+    if string.find(arg1, " crit ") or string.find(arg1, " crits ")or string.find(arg1, " critically ") then
+      local idx = math.random(1, number_of_sounds)
+      PlaySoundFile("Interface\\AddOns\\Kikicrit\\sounds2\\wow-"..idx..".mp3")
     end
   end
 end)
